c6e8b5722dee635fb590ebfbbe79012f30879b7d
Sourabh Yadav <sourabhy4441@gmail.com>
2026-08-04 22:31:30 +0530

    Rename Main class to Leedcode20

---
+++ Leedcode20.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Leedcode20{
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter parentheses string: ");
+        String s = sc.nextLine();
+
+        if (isValid(s)) {
+            System.out.println("Valid Parentheses");
+        } else {
+            System.out.println("Invalid Parentheses");
+        }
+
+        sc.close();
+    }
+}
