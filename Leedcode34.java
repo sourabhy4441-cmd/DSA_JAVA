714bfdfb395b13b9ac38c44b43d0f676f7378ad4
Sourabh Yadav <sourabhy4441@gmail.com>
2026-08-06 22:58:26 +0530

    Add searchRange method to find target positions
    Implement binary search to find the first and last positions of a target in a sorted array.

---
+++ Leedcode34.java
@@ -0,0 +1,63 @@
+public class Leedcode34 {
+
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    // Find the first occurrence of target
+    private static int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1; // Continue searching on the left
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Find the last occurrence of target
+    private static int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1; // Continue searching on the right
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+
+        int[] result = searchRange(nums, target);
+
+        System.out.println("First Position: " + result[0]);
+        System.out.println("Last Position: " + result[1]);
+    }
+}
